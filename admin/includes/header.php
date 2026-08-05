c09d94db6eafe384e49deb64749f36e104466aff
Kartikey Lodha <kartikeylodha567@gmail.com>
2026-08-05 13:19:19 +0530

    new

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>School ERP · Staff & Payroll</title>
-    <link rel="icon" type="image/png" href="../../images/logo.jpeg">   
+    <link rel="icon" type="../../images/logo.jpeg" href="../../images/logo.jpeg">   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="style/ok.css">
